778ca0cb538d6d292e0013de8da6186bb6cb0237
Maxwell Tavares <maxwelltrt0709@gmail.com>
2024-11-04 13:35:42 -0400

    + Se agregó el calendario.
    * Se modificó la visual del Dashboard.

---
+++ Aspire POS/Controllers/CalendarController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aspire_POS.Controllers
+{
+    public class CalendarController : Controller
+    {
+        public IActionResult Index()
+        {
+            InitializeViewBags(false, false, false);
+            return View();
+        }
+
+        void InitializeViewBags(bool _navbar, bool _sidebar, bool _footer)
+        {
+            ViewBag.HideNavbar = _navbar;
+            ViewBag.HideSidebar = _sidebar;
+            ViewBag.HideFooter = _footer;
+        }
+    }
+}
