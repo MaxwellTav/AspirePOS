610d80b461357e6113789ffa31002d8d634b3f0d
Maxwell Tavares <maxwelltrt0709@gmail.com>
2024-11-10 18:20:16 -0400

    + Controlador para el personal "StaffController".
    + Se agregó la vista del Staff.
    
    + Se agrego el controlador para las configuraciones "SettingsController".
    
    + Se agregó la vista de Settings.
    
    * Se modificó:
       • Vista de calendario.
       • Vista _Layout.
       • Vista inicial / prueba.
    
    Finalización de la configuración básica de la vista.

---
+++ Aspire POS/Controllers/StaffController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aspire_POS.Controllers
+{
+    public class StaffController : Controller
+    {
+        public IActionResult Index()
+        {
+            InitializeViewBags(false, false, false);
+            return View();
+        }
+
+        /// <summary>
+        /// Automáticamente oculta o muestra un componente de la página.
+        /// </summary>
+        /// <param name="_navbar">Esconderá la barra de navegación superior.</param>
+        /// <param name="_sidebar">Esconderá la barra lateral, la cuál está el menú y demás opciones.</param>
+        /// <param name="_footer">Esconderá el pie de página.</param>
+        void InitializeViewBags(bool _navbar, bool _sidebar, bool _footer)
+        {
+            ViewBag.HideNavbar = _navbar;
+            ViewBag.HideSidebar = _sidebar;
+            ViewBag.HideFooter = _footer;
+        }
+    }
+}
